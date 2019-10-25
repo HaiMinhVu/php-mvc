1ab324f6574c017c92ddc3b64819b8bc448a6016
Hai Vu <hvu@sellmark.net>
2019-10-25 15:55:52 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+require_once './mvc/bridge.php';
+
+//echo 1;
+$a = new App();
+
+//echo $_GET['url'];
+?>
